a0796a041c0934225af51bc103e2e6e0db991f88
romagoel <romagoel21@gmail.com>
2015-03-09 05:18:22 -0700

    server updated

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ res.sendFile(__dirname + '/client');
 
 */
 
-mongoose.connect('mongodb://localhost:27017/mean-demo');
+mongoose.connect('mongodb://abc:abc@ds045998.mongolab.com:45998/mean-demo');
 app.use(bodyParser());
 console.log('value of ******** dirname: ', __dirname);
 app.use(express.static(__dirname +'/client'));
@@ -22,9 +22,6 @@ app.use(express.static(__dirname +'/client'));
 app.get('/api/meetups' ,meetupscontroller.list);
 app.post('/api/meetups' ,meetupscontroller.create);
 
-app.listen(3000,function()
-{
-console.log('I\'m Listening ...  ');
-}
-);
+app.listen(process.env.PORT || 5000);
+console.log("Server running on port no. 5000");
 
